324f8dd06f4fb3ded9180b2c8a033f514f82fa23
mtaffetani <93777374+mtaffetani@users.noreply.github.com>
2025-08-28 18:12:58 +0000

    Deploying to gh-pages from @ mtaffetani/mtaffetani.github.io@071d46e4d4e941cf5c79acf1ab391c3900bd3faa 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,19 +9,19 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-projects-and-interests",
-          title: "Projects and Interests",
-          description: "A growing collection of your cool projects.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/projects/";
-          },
-        },{id: "nav-publications",
+  },{id: "nav-publications",
           title: "Publications",
           description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/publications/";
+          },
+        },{id: "nav-projects-and-interests",
+          title: "Projects and Interests",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/projects/";
           },
         },{id: "nav-cv",
           title: "cv",
