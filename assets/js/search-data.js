be64ca2529e7d92e045cddcb75f98d4dc7b09f22
mtaffetani <93777374+mtaffetani@users.noreply.github.com>
2025-08-28 18:30:06 +0000

    Deploying to gh-pages from @ mtaffetani/mtaffetani.github.io@e0e9547feccb71436d0b31fb11d88969f55b06bb 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -3,8 +3,8 @@ const ninja = document.querySelector('ninja-keys');
 
 // add the home and posts menu items
 ninja.data = [{
-    id: "nav-about",
-    title: "about",
+    id: "nav-morphing-systems-and-soft-matter",
+    title: "Morphing Systems and Soft Matter",
     section: "Navigation",
     handler: () => {
       window.location.href = "/";
