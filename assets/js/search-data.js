1ebb881b4d38051fd56e3343d989949cabb23e55
mtaffetani <93777374+mtaffetani@users.noreply.github.com>
2025-09-01 19:35:50 +0000

    Deploying to gh-pages from @ mtaffetani/mtaffetani.github.io@2133c074b234578984acdc2f668bbaa88a394fc2 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -412,16 +412,16 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-abstract-submission-for-the-workshop-slender-and-active-mechanics-of-emerging-materials-and-systems-in-edinburgh-on-the-4th-of-december-2025-is-now-open",
-          title: 'Abstract submission for the workshop Slender and Active: Mechanics of Emerging Materials and...',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
+            },},{id: "news-a-long-announcement-with-details",
           title: 'A long announcement with details',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/Xannouncement_2/";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          description: "",
+          section: "News",},{id: "news-abstract-submission-for-the-workshop-slender-and-active-mechanics-of-emerging-materials-and-systems-in-edinburgh-on-the-4th-of-december-2025-is-now-open",
+          title: 'Abstract submission for the workshop Slender and Active: Mechanics of Emerging Materials and...',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
