66aa7b4c427465c131316f632e95da30b571cd88
mtaffetani <93777374+mtaffetani@users.noreply.github.com>
2025-09-01 19:34:21 +0000

    Deploying to gh-pages from @ mtaffetani/mtaffetani.github.io@d916e03f16d503c66388170b9723a3c5ac91b99b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -419,7 +419,7 @@ ninja.data = [{
           title: 'A long announcement with details',
           description: "",
           section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
+              window.location.href = "/news/Xannouncement_2/";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
