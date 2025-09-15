b0bc036774fbdce9681f69a91123536dae66ad68
mtaffetani <93777374+mtaffetani@users.noreply.github.com>
2025-09-15 15:59:31 +0000

    Deploying to gh-pages from @ mtaffetani/mtaffetani.github.io@96c48f47e843902c31ad21788ea8b6bded34587a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -423,11 +423,6 @@ ninja.data = [{
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/Xannouncement_2/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/2_project/";
             },},{id: "projects-project-3-with-very-long-name",
           title: 'project 3 with very long name',
           description: "a project that redirects to another website",
@@ -463,6 +458,11 @@ ninja.data = [{
           description: "Emerging shapes in novel systems",
           section: "Projects",handler: () => {
               window.location.href = "/projects/Project_ActiveSolids/";
+            },},{id: "projects-localization",
+          title: 'Localization',
+          description: "How localizations forms in morphing thin objects and their interactions",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/Project_Localizations/";
             },},{id: "projects-active-solids",
           title: 'Active Solids',
           description: "Morphing in Slender Structure",
