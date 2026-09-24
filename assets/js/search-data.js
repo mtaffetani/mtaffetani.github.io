2f72791bae81a04336f1ad3adb42320c0de14350
mtaffetani <93777374+mtaffetani@users.noreply.github.com>
2026-09-24 16:37:26 +0000

    Deploying to gh-pages from @ mtaffetani/mtaffetani.github.io@885efd9a1d5ad998746ba21ad65bb7d312073f5b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -3,8 +3,8 @@ const ninja = document.querySelector('ninja-keys');
 
 // add the home and posts menu items
 ninja.data = [{
-    id: "nav-tm3",
-    title: "TM3",
+    id: "nav-tl",
+    title: "TL",
     section: "Navigation",
     handler: () => {
       window.location.href = "/";
